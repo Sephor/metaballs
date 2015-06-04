15e8d2ebfc7dfe4fcdac21943185b7bdaa327d77
Moritz Eissenhauer <moritz.eissenhauer@gmail.com>
2015-06-04 17:11:14 +0200

    Einfache gravitation

--- source/metaballsexample/FluidSimulator.cpp
+++ source/metaballsexample/FluidSimulator.cpp
@@ -1,12 +1,36 @@
 #include "FluidSimulator.h"
 
-FluidSimulator::FluidSimulator()
+FluidSimulator::FluidSimulator() : m_gravConstant(0.f, -.1f, 0.f)
 {
-
+	m_metaballs = std::vector<Metaball>();
+	for (int i = 0; i < 20; i++)
+	{
+		Metaball m;
+		m.position = glm::vec3(i * 0.75f, .0f, .0f);
+		m.radius = 1.f;
+		m.velocity = glm::vec3(.0f);
+		m_metaballs.push_back(m);
+	}
 }
 
 
 FluidSimulator::~FluidSimulator()
 {
 
+}
+
+const std::vector<FluidSimulator::Metaball> & FluidSimulator::getMetaballs() const
+{
+	return m_metaballs;
+}
+
+void FluidSimulator::update(float elapsedTime)
+{
+	elapsedTime = elapsedTime > .05f ? .05f : elapsedTime;
+
+	for (auto & metaball : m_metaballs)
+	{
+		metaball.velocity += m_gravConstant * elapsedTime;
+		metaball.position += metaball.velocity * elapsedTime;
+	}
 }
--- source/metaballsexample/FluidSimulator.h
+++ source/metaballsexample/FluidSimulator.h
@@ -1,8 +1,26 @@
 #pragma once
 
+#include <vector>
+
+#include <glm/vec3.hpp>
+
 class FluidSimulator
 {
 public:
 	FluidSimulator();
 	~FluidSimulator();
+	
+	struct Metaball
+	{
+		float radius;
+		glm::vec3 position;
+		glm::vec3 velocity;
+	};
+
+	const std::vector<Metaball> & getMetaballs() const;
+	void update(float elapsedTime);
+
+private:
+	const glm::vec3 m_gravConstant;
+	std::vector<Metaball> m_metaballs;
 };
