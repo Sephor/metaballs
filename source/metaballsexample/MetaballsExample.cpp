f635c9566bc864e844464e2970a5d80bb5bd744c
Jan van Dieken <Zanthelo@users.noreply.github.com>
2015-06-04 23:20:24 +0200

    added timer for fluid simulation

--- source/metaballsexample/MetaballsExample.cpp
+++ source/metaballsexample/MetaballsExample.cpp
@@ -105,7 +105,7 @@ void MetaballsExample::onInitialize()
 
 void MetaballsExample::onPaint()
 {
-	m_fluidSimulator.update(0.01f); //TODO: get actual elapsed time
+	m_fluidSimulator.update();
 
 	if (m_viewportCapability->hasChanged())
 	{
